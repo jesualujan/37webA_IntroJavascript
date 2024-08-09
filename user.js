349e0b0bdd83a990ffccff9ddfb0061c9e2ceb0c
jesualujan <lujan.oficial@gmail.com>
2024-08-08 21:19:53 -0600

    ✨ pedir información al usuario

---
+++ user.js
@@ -0,0 +1,6 @@
+// pedirle información al usuario
+   //camelCase
+let fullName = prompt("¿Cuál es tu nombre?")
+let age = prompt("¿Cuál es tu edad?")
+console.log("¡Hola! " + fullName + " saludos, tengo " + age + " años")
+document.write("¡Hola! " + fullName + " saludos, tengo " + age + " años")
