372306ad24e342ab24d88539e8356135a6737e4a
jesualujan <lujan.oficial@gmail.com>
2024-08-13 21:07:53 -0600

    ✨ los guarde en su respectiva carpeta

---
+++ 01.Variables/main.js
@@ -0,0 +1,39 @@
+// estos son comentarios en javascript
+// aquí va nuestro código de javascript 
+console.log('Mostar un mensaje en la consola')
+document.write('Mostrar un mensaje en el documento html')
+alert('despliega un mensaje en forma de alerta')
+
+// DECLARACION DE VARIABLES CON VAR:
+var nombre = "Jesua Luján"
+var edad = 30 // esto es un número
+var estaVivo = true // true = verdadero  false = falso 
+var estatura = 172
+var peso = 75
+var ciudad = "Oaxaca" // esto es un texto
+var pais = "México"
+
+console.log(nombre,edad,estaVivo)
+
+// variables de tipo let 
+let name = "Patricia"
+let age = 30
+let city = "Tulum"
+
+console.log(name) // Imprime "Patricia"
+console.log(age) // Imprime 30
+console.log(city) // Imprime "Tulum"
+console.log(name,age,city)
+// concatenación
+console.log("Hola mi nombres es " + name + " tengo " + age + " y vivo en " + city + ' 🚀')
+console.error("esto es un mensaje de error en la consola")
+console.warn('Warning: esto es una advertencia');
+console.log(
+    "%caprendiendo javascript! 🚀",
+    "color:yellow;font-family:system-ui;font-size:2rem;-webkit-text-stroke: 1px black;font-weight:bold"
+  );
+
+  // 22 formas de trabajar o usar la consola:
+  // https://dev.to/franciscomoretti/22-useful-console-methods-every-web-developer-should-know-57le
+
+
