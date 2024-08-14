2701c18ed53df486cbac6d36b5a3f8e78beb164b
jesualujan <lujan.oficial@gmail.com>
2024-08-13 21:07:35 -0600

    💩 archivos eliminados por no tener carpeta

--- user.js
+++
@@ -1,6 +0,0 @@
-// pedirle información al usuario
-   //camelCase
-let fullName = prompt("¿Cuál es tu nombre?")
-let age = prompt("¿Cuál es tu edad?")
-console.log("¡Hola! " + fullName + " saludos, tengo " + age + " años")
-document.write("¡Hola! " + fullName + " saludos, tengo " + age + " años")
